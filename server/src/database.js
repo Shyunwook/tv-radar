d864f4030f820d4ff14bca980415ae46030b863e
admin <admin@MAC-9852-0000.local>
2019-03-17 15:01:58 +0900

    dependencies 수정 후 배포 완료 버전

--- server/src/database.js
+++ server/src/database.js
@@ -1,6 +1,6 @@
 import redis from 'redis';
 
 module.exports = (() => {
-  return redis.createClient(6379,'172.31.29.112');
-  // return redis.createClient(6379,'127.0.0.1');
+  // return redis.createClient(6379,'172.31.29.112');
+  return redis.createClient(6379,'127.0.0.1');
 })();
