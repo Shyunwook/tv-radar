f4c8502c616dc6c785e7c3ec89c385d35c5e9647
admin <admin@MAC-9852-0000.local>
2019-03-18 09:21:36 +0900

    데이터베이스 설정

--- build/src/database.js
+++ build/src/database.js
@@ -5,6 +5,5 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 var _redis = _interopRequireDefault(require("redis"));
 
 module.exports = function () {
-  // return redis.createClient(6379,'172.31.29.112');
-  return _redis.default.createClient(6379, '127.0.0.1');
+  return _redis.default.createClient(6379, '172.31.29.112'); // return redis.createClient(6379,'127.0.0.1');
 }();
