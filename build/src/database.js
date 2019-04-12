54d92f1eb8fea7469a6e44a23b76f79234ad8705
admin <admin@MAC-9852-0000.local>
2019-04-12 11:14:22 +0900

    redis ip 수정

--- build/src/database.js
+++ build/src/database.js
@@ -5,5 +5,5 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 var _redis = _interopRequireDefault(require("redis"));
 
 module.exports = function () {
-  return _redis.default.createClient(6379, '172.31.29.112'); // return redis.createClient(6379,'127.0.0.1');
+  return _redis.default.createClient(6379, '13.209.15.25'); // return redis.createClient(6379,'127.0.0.1');
 }();
--- server/src/database.js
+++ server/src/database.js
@@ -1,6 +1,6 @@
 import redis from 'redis';
 
 module.exports = (() => {
-  return redis.createClient(6379,'172.31.29.112');
+  return redis.createClient(6379,'13.209.15.25');
   // return redis.createClient(6379,'127.0.0.1');
 })();
